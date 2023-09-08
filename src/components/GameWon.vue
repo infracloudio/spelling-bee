ef611c9ef2d421681acd31a89ce5d3e117bede20
Bhavin Gandhi <bhavin@infracloud.io>
2023-09-08 17:43:59 +0530

    Have only the messages and use el-loader
    Signed-off-by: Bhavin Gandhi <bhavin@infracloud.io>

--- src/components/GameWon.vue
+++ src/components/GameWon.vue
@@ -1,35 +1,23 @@
 <script setup lang="ts">
 import { useMainStore } from "../store";
-import { Share } from '@element-plus/icons-vue'
+import { Share } from "@element-plus/icons-vue";
 
 import axios from "axios";
-import { ref } from 'vue';
+import { ref } from "vue";
 import messages from "../../data/shareMessage.json";
-const store = useMainStore();
-let shareButton = ref("Share");
-const shareScore = async () => {
-  // Making a POST API call using Axios
-  try {
-    const randomNumber = Math.floor(Math.random() * 10);
+import { ElMessage } from "element-plus";
 
-    shareButton.value = `<svg viewBox="0 0 200 50" width="auto" height="auto">
-                            <!-- Background -->
-                            <rect x="0" y="0" width="200" height="50" fill="#fce303"></rect>
-                            
-                            <!-- Loader Circles -->
-                            <circle id="dot1" cx="10" cy="25" r="10" fill="#4c51bf">
-                              <animate attributeName="cx" dur="1.5s" repeatCount="indefinite" values="10; 190; 10"></animate>
-                            </circle>
-                            <circle id="dot2" cx="10" cy="25" r="10" fill="#4c51bf">
-                              <animate attributeName="cx" dur="1.5s" repeatCount="indefinite" values="10; 190; 10" begin="0.3s"></animate>
-                            </circle>
-                            <circle id="dot3" cx="10" cy="25" r="10" fill="#4c51bf">
-                              <animate attributeName="cx" dur="1.5s" repeatCount="indefinite" values="10; 190; 10" begin="0.6s"></animate>
-                            </circle>
-                            <circle id="dot4" cx="10" cy="25" r="10" fill="#4c51bf">
-                              <animate attributeName="cx" dur="1.5s" repeatCount="indefinite" values="10; 190; 10" begin="0.9s"></animate>
-                            </circle>
-                          </svg>`;
+const store = useMainStore();
+const loading = ref(false);
+
+const shareScore = async () => {
+  loading.value = true;
+
+  const textToShare = messages[
+    Math.floor(Math.random() * messages.length)
+  ].replace("<SCORE>", store.getUserScore);
+
+  try {
     const response = await axios.post(
       import.meta.env.VITE_GSA_URL || "",
       {
@@ -44,50 +32,68 @@ const shareScore = async () => {
       }
     );
     console.log("Data sent successfully:", response.data);
-    // Example functionality for the share button
-    const score = store.getUserScore
+
     if (navigator.share) {
       navigator.share({
-        title: messages[randomNumber].title,
-        text: (messages[randomNumber].text).replace("<SCORE>", score.toString()),
-        url: window.location.href,
+        text: textToShare,
       });
     } else {
-      alert("Copied to clipboard!📋");
-      const mytext = `${messages[randomNumber].title}\n${(messages[randomNumber].text).replace("<SCORE>", score.toString())}\n${window.location.href}`;
-      await navigator.clipboard.writeText(mytext);
+      ElMessage({
+        duration: 2000,
+        appendTo: "#app",
+        customClass: "toast-message",
+        grouping: true,
+        showClose: true,
+        type: "success",
+        message: "Copied to clipboard! 📋",
+      });
+      navigator.clipboard.writeText(textToShare);
     }
   } catch (error) {
     console.error("Error sending data:", error);
+    ElMessage({
+      duration: 2000,
+      appendTo: "#app",
+      customClass: "toast-message",
+      grouping: true,
+      showClose: true,
+      type: "warning",
+      message: "Error submitting score, try again!",
+    });
   }
-
-  shareButton.value = `Share`;
+  loading.value = false;
 };
-
 </script>
 
 <template>
   <div>
-     <p>
-     <h2>Your Score: {{ store.getUserScore }} 🎉</h2>
-     <p>Well done! You're doing great.</p>
-     <p>🚀 Share the results with your friends on Twitter/LinkedIn and win some cool swags at the InfraCloud booth! 🎁🌟
-     </p>
-     <p>
-        And stand a chance to win an electric
-        <strong>hoverboard! 💨</strong>, do keep an 👁️ on your social media platform! 😉🏆
-     <p>
-        <img height="90" src="../assets/hoverboard-400.png" alt="Mega-prize" class="hoverboard-icon" />
-     </p>
-     <el-button @click="shareScore" style="height: 3rem; margin-top: 1rem; width: 75%; font-size: 18px;">
-        <el-icon
-           class="el-icon--left">
-           <Share />
-        </el-icon>
-        <strong>Share</strong>
-     </el-button>
-     </p>
-     </p>
+    <h2>Your Score: {{ store.getUserScore }} 🎉</h2>
+    <p>Well done! You're doing great.</p>
+    <p>
+      🚀 Share the results with your friends on Twitter/LinkedIn and win some
+      cool swags at the InfraCloud booth! 🎁🌟
+    </p>
+    <p>
+      And stand a chance to win an electric
+      <strong>hoverboard! 💨</strong>, do keep an 👁️ on your social media
+      platform! 😉🏆
+    </p>
+    <p>
+      <img
+        height="90"
+        src="../assets/hoverboard-400.png"
+        alt="Mega-prize"
+        class="hoverboard-icon" />
+    </p>
+    <el-button
+      v-loading="loading"
+      @click="shareScore"
+      style="height: 3rem; margin-top: 1rem; width: 75%; font-size: 18px">
+      <el-icon class="el-icon--left">
+        <Share />
+      </el-icon>
+      <strong>Share</strong>
+    </el-button>
   </div>
 </template>
 
